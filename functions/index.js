b19519e16530f2ff73167a9a12dbdafb02851cc4
Andrew Still <astill@dragonaere.com>
2025-02-08 12:23:55 -0500

    Maybe fixed?

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,17 @@
 export default {
   async fetch(request) {
-      let url = new URL(request.url);
+    let url = new URL(request.url);
 
-      if (url.pathname.startsWith("/forum")) {
-          // Proxy requests to XenForo at forum.dragonaere.net
-          return fetch("https://forum.dragonaere.net" + url.pathname + url.search, {
-              headers: request.headers
-          });
-      }
-
-      // Serve React site for everything else
-      return fetch("https://www.dragonaere.net" + url.pathname + url.search, {
-          headers: request.headers
+    if (url.pathname.startsWith("/forum")) {
+      // Proxy requests to XenForo at forum.dragonaere.net
+      return fetch("https://forum.dragonaere.net" + url.pathname + url.search, {
+        headers: request.headers
       });
+    }
+
+    // Serve React static site for everything else
+    return fetch("https://www.dragonaere.net" + url.pathname + url.search, {
+      headers: request.headers
+    });
   }
 };
