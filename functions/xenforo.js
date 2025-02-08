a6a5c2bdc78547aba5161b98e1bd91b445a7bbc5
Andrew Still <astill@dragonaere.com>
2025-02-08 12:03:53 -0500

    Test

---
+++ functions/xenforo.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request) {
+      let url = new URL(request.url);
+
+      if (url.pathname.startsWith("/forum")) {
+          // Proxy requests to XenForo at forum.dragonaere.net
+          return fetch("https://forum.dragonaere.net" + url.pathname + url.search, {
+              headers: request.headers
+          });
+      }
+
+      // Serve React site for everything else
+      return fetch("https://www.dragonaere.net" + url.pathname + url.search, {
+          headers: request.headers
+      });
+  }
+};
